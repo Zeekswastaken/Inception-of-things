164d95c758284f7d519c47e17861dd4358cfec5e
mobo-9-0045 <abdelilahoman@gmail.com>
2025-01-11 20:19:09 -0600

    updating argocd to be in k3d container instead of default context

--- bonus/k3d.sh
+++ bonus/k3d.sh
@@ -1,22 +1,40 @@
 #installing k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
+k3d cluster create bonus-cluster --api-port 8765 -p 8081:80@loadbalancer  -p 8888:8888@loadbalancer --agents 2 --wait
+
 #creating a namespace for agrocd
-sudo kubectl create namespace argocd
+
+export KUBECONFIG=$(k3d kubeconfig write bonus-cluster)
+
+kubectl config current-context
+
+kubectl create namespace argocd
 
 #this dev namesapace where i will deploy my app using argocd
-sudo kubectl create namespace dev
+kubectl create namespace dev
+
 
 #install argocd in k3d cluster
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# kubectl wait --for=condition=Ready pods --all -n argocd
+kubectl wait --for=condition=Ready pods --all -n argocd --timeout=300s
+kubectl -n argocd patch secret argocd-secret \
+  -p '{"stringData": {
+    "admin.password": "$2a$12$xyk8mlgC6l6gWQhTA.LF8uqlX5ng6Ju5BU7zhJ4Sp4VuCzQT7szIm",
+    "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+  }}'
 
-echo "checking agrocd status"
-sudo kubectl get pods -n argocd
+echo "kubectl apply -f ./argo-application.yaml -n argocd"
+kubectl apply -f ./argo-application.yaml -n argocd
+echo "kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'"
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+echo "kubectl get pods -n argocd"
+kubectl get pods -n argocd
+kubectl wait --for=condition=Ready pods --all -n argocd
 
-echo "Username: amdin"
-echo "Password: yU1QJ6s4MUYYfaID"
-echo "Expose the ArgoCD API server locally, then try to accessing the dashboard"
-sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
+echo "Username: admin"
+echo "forwarding to argocd"
+kubectl port-forward svc/argocd-server -n argocd 8082:443 2>&1 >/dev/null &
 
-sudo kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
-
+# kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
