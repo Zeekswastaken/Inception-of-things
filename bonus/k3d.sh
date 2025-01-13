ece65811dca51d7e8c2d9fa8b81e88d0b5f1dad4
mobo-9-0045 <abdelilahoman@gmail.com>
2025-01-12 19:57:03 -0600

    adding git lab to k3d bonus-cluster

--- bonus/k3d.sh
+++ bonus/k3d.sh
@@ -6,6 +6,7 @@ k3d cluster create bonus-cluster --api-port 8765 -p 8081:80@loadbalancer  -p 888
 #creating a namespace for agrocd
 
 export KUBECONFIG=$(k3d kubeconfig write bonus-cluster)
+sleep 10
 
 kubectl config current-context
 
@@ -14,8 +15,10 @@ kubectl create namespace argocd
 #this dev namesapace where i will deploy my app using argocd
 kubectl create namespace dev
 
+#creating name space for gitlab
+# kubectl create namespace gitlab
 
-#install argocd in k3d cluster
+#install argocd in k3d cluster this will aply in argocd namespace (sub-cluster in bonus-cluster)
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # kubectl wait --for=condition=Ready pods --all -n argocd
 kubectl wait --for=condition=Ready pods --all -n argocd --timeout=300s
@@ -34,7 +37,11 @@ kubectl get pods -n argocd
 kubectl wait --for=condition=Ready pods --all -n argocd
 
 echo "Username: admin"
+echo "Password: 123"
 echo "forwarding to argocd"
-kubectl port-forward svc/argocd-server -n argocd 8082:443 2>&1 >/dev/null &
+kubectl port-forward svc/argocd-server -n argocd 9090:443 2>&1 >/dev/null &
+
+sleep 5
+export KUBECONFIG=$(k3d kubeconfig write bonus-cluster)
 
 # kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
