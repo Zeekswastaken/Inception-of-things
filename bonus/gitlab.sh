37e7a3490c340bdc82dadb37d7bfa8a98a3073cb
mobo <mobo@Mobo.myguest.virtualbox.org>
2025-01-15 15:16:20 +0100

    fixed gitlab instance

--- bonus/gitlab.sh
+++ bonus/gitlab.sh
@@ -1,17 +1,15 @@
 #helm installation
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
+BLUE='\033[0;34m'
+NC='\033[0m'
+info() { echo -e "${BLUE}[INFO] $1${NC}"; }
+
+info "Exporting k3d config cluster to kubectl config"
 export KUBECONFIG=$(k3d kubeconfig write bonus-cluster)
+
+info "creating gitlab name spcace"
 kubectl create namespace gitlab
-# create namespace for gitlab
-
-
-sudo sed -i '/gitlab.localhost/d' /etc/hosts
-sudo sed -i '/minio.localhost/d' /etc/hosts
-sudo sed -i '/registry.localhost/d' /etc/hosts
-sudo sed -i '/kas.localhost/d' /etc/hosts
-echo "127.0.0.1 gitlab.localhost minio.localhost registry.localhost kas.localhost" | sudo tee -a /etc/hosts
-#set this space to gitlab
 
 #downloading and installing gitlab to thiss cluster
 
@@ -21,12 +19,19 @@ kubectl delete pvc --all -n gitlab 2>/dev/null || true
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
 
-helm install gitlab gitlab/gitlab \
-  --set global.hosts.domain=gitlab.localhost \
-  --set certmanager-issuer.email=me@example.com
+info "Installing gitlab ..."
+helm upgrade --install gitlab gitlab/gitlab \
+  --namespace gitlab \
+  --timeout 600s \
+  --values gitlab-values.yaml \
 
-kubectl port-forward services/gitlab-nginx-ingress-controller 8082:443 -n gitlab --address="0.0.0.0" 2>&1 > /var/log/gitlab-webserver.log &
+info "Waiting for services to be ready..."
+kubectl wait --namespace gitlab --for=condition=ready pod -l app=webservice --timeout=600s || true
 
-echo "geting gitlab passwor"
-kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
+kubectl patch svc gitlab-webservice-default -n gitlab -p '{"spec": {"type": "LoadBalancer"}}'
+
+kubectl port-forward services/gitlab-webservice-default 80:8181 -n gitlab
+
+info "Gitlab root password: "
+GITLAB_PASSWORD=$(kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -ojsonpath='{.data.password}' | base64 --decode)
 
